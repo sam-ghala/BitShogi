9e7439cb258fbf390fec721feda6c4acd0174ae8
sam-ghala <ghalayini.sj@gmail.com>
2025-12-05 20:19:06 +0100

    removed const

--- BitShogi/server/server.jl
+++ BitShogi/server/server.jl
@@ -213,7 +213,7 @@ function handle_claude_move(req::HTTP.Request)
         end
         
         # Build prompt for Claude
-        const adjustedSfen = sfen.replace('w', 'b');
+        adjustedSfen = sfen.replace('w', 'b');
         prompt = """You are playing minishogi (5x5 Japanese chess) as Black against a human player.
 
             Current position (SFEN notation): $adjustedSfen
